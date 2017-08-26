1e1f84f1e146e2878163a9d518ecc83c9c2d3c12
khanhkiem <buidoigiauco@gmail.com>
2017-08-26 22:15:42 +0700

    Merge remote-tracking branch 'origin/Lab1_test'

--- Lab1/Exercises/graph.py
+++ Lab1/Exercises/graph.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 serial_version = [0.004, 0.055, 10.185, 720.354, 1284.211]
 
 # results from serial version of matrix multiplication with size of matrices: 10, 100, 1000, 10000, 20000, 30000, 40000, 50000, ...
-# pthread_version = []
+pthread_version = [0.004, 0.040, 5.0,300,500]
 
 matrix_sizes = ('10', '100', '1000', '10000', '20000')
 x_pos = np.arange(len(serial_version))
 
 plt.plot(serial_version, label = 'Serial Version')
-# plt.plot(pthread_version, label = 'Pthread Version')
+plt.plot(pthread_version, label = 'Pthread Version')
 
 plt.xlabel('Matrix sizes (N)')
 plt.xticks(x_pos, matrix_sizes)
