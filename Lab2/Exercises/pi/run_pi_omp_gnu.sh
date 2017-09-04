fa9ac483e156a4e0c9924b7b1fa165ffa6b36c8d
khanhkiem <buidoigiauco@gmail.com>
2017-09-04 19:01:28 +0700

    Remove latest output file to ensure robustness

--- Lab2/Exercises/pi/run_pi_omp_gnu.sh
+++ Lab2/Exercises/pi/run_pi_omp_gnu.sh
@@ -4,15 +4,24 @@
 # Executed program and this bash must be in same directory
 
 program='pi_omp_gnu'
-output_dir='output_'$program
+output_dir=`dirname $0`/'output_'$program
 
-mkdir -p `dirname $0`/$output_dir
+mkdir -p $output_dir
+
+# find latest output file
+latest_output_file=$output_dir/`ls -t $output_dir/ | head -1`
+# delete latest file, because it may be uncompleted output
+# ensure that there is a file exist in output_dir
+if [ "$latest_output_file" != "$output_dir/" ]
+then
+  rm $latest_output_file
+fi
 
 for T in 1 8 16 32 48
 do 
   for N in 1000000 10000000 100000000 200000000 300000000 400000000 500000000
   do
-    process_output_dir=`dirname $0`/$output_dir/$program-$N-$T
+    process_output_dir=$output_dir/$program-$N-$T
     
     if [ -e $process_output_dir ]
     then
--- Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
+++ Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
@@ -4,15 +4,26 @@
 # Executed program and this bash must be in same directory
 
 program='sgemm_omp_gnu'
-output_dir='output_'$program
 
-mkdir -p `dirname $0`/$output_dir
+output_dir=`dirname $0`/'output_'$program
+
+mkdir -p $output_dir
+
+# find latest output file
+latest_output_file=$output_dir/`ls -t $output_dir/ | head -1`
+# delete latest file, because it may be uncompleted output
+# ensure that there is a file exist in output_dir
+if [ "$latest_output_file" != "$output_dir/" ]
+then
+  printf "\nContinue previous work\n"
+  rm $latest_output_file
+fi
 
 for T in 1 8 16 32 48
 do
   for N in 100 1000 2000 4000 6000 8000 10000
   do
-    process_output_dir=`dirname $0`/$output_dir/$program-$N-$T
+    process_output_dir=$output_dir/$program-$N-$T
     
     if [ -e $process_output_dir ]
     then
