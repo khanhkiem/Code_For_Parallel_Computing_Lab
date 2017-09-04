19e12cf31324139c08db4ea42610e164194635ee
khanhkiem <buidoigiauco@gmail.com>
2017-09-04 17:55:13 +0700

    print call process

--- Lab2/Exercises/pi/run_pi_omp_gnu.sh
+++ Lab2/Exercises/pi/run_pi_omp_gnu.sh
@@ -4,14 +4,16 @@
 # Executed program and this bash must be in same directory
 
 program='pi_omp_gnu'
+output_dir='output_'$program
 
-mkdir -p `dirname $0`/output
+mkdir -p `dirname $0`/$output_dir
 
 for T in 1 8 16 32 48
 do 
   for N in 1000000 10000000 100000000 200000000 300000000 400000000 500000000
   do
     printf '\n********************\n\n'
-    `dirname $0`/$program $N $T  | tee `dirname $0`/output/$program-$N-$T
+    printf "$program $N $T\n\n"
+    `dirname $0`/$program $N $T  | tee `dirname $0`/$output_dir/$program-$N-$T
   done
 done
--- Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
+++ Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
@@ -4,14 +4,16 @@
 # Executed program and this bash must be in same directory
 
 program='sgemm_omp_gnu'
+output_dir='output_'$program
 
-mkdir -p `dirname $0`/output
+mkdir -p `dirname $0`/$output_dir
 
 for T in 1 8 16 32 48
 do
   for N in 100 1000 2000 4000 6000 8000 10000
   do
     printf "\n********************\n\n"
-    `dirname $0`/$program $N $T  | tee `dirname $0`/output/$program-$N-$T
+    printf "$program $N $T\n\n"
+    `dirname $0`/$program $N $T  | tee `dirname $0`/$output_dir/$program-$N-$T
   done
 done
