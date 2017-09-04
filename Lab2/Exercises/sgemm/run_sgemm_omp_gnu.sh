0ba1d8f33820511dfbaaf01b471de079c70bb77a
khanhkiem <buidoigiauco@gmail.com>
2017-09-04 16:28:25 +0700

    Refactor and add print terminal and save output

--- Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
+++ Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
@@ -3,15 +3,15 @@
 # 'dirname $0' return the directory path where this bash stored
 # Executed program and this bash must be in same directory
 
-program='pi_omp_gnu'
+program='sgemm_omp_gnu'
 
 mkdir -p `dirname $0`/output
 
 for T in 1 8 16 32 48
-do 
-  for N in 1000000 10000000 100000000 200000000 300000000 400000000 500000000
+do
+  for N in 100 1000 2000 4000 6000 8000 10000
   do
-    printf '\n********************\n\n'
+    printf "\n********************\n\n"
     `dirname $0`/$program $N $T  | tee `dirname $0`/output/$program-$N-$T
   done
 done
