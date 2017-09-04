c7c318b5cf2b5eae53c0c67e0abb3bbf8d5fad86
khanhkiem <buidoigiauco@gmail.com>
2017-09-04 19:04:00 +0700

    refactor output_dir/ -> output_dir

--- Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
+++ Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
@@ -5,15 +5,15 @@
 
 program='sgemm_omp_gnu'
 
-output_dir=`dirname $0`/'output_'$program
+output_dir=`dirname $0`/'output_'$program/
 
 mkdir -p $output_dir
 
 # find latest output file
-latest_output_file=$output_dir/`ls -t $output_dir/ | head -1`
+latest_output_file=$output_dir`ls -t $output_dir | head -1`
 # delete latest file, because it may be uncompleted output
 # ensure that there is a file exist in output_dir
-if [ "$latest_output_file" != "$output_dir/" ]
+if [ "$latest_output_file" != "$output_dir" ]
 then
   printf "\nContinue previous work\n"
   rm $latest_output_file
@@ -23,7 +23,7 @@ for T in 1 8 16 32 48
 do
   for N in 100 1000 2000 4000 6000 8000 10000
   do
-    process_output_dir=$output_dir/$program-$N-$T
+    process_output_dir=$output_dir$program-$N-$T
     
     if [ -e $process_output_dir ]
     then
