992752aae5b6cdf31c3253ec2b029251c801e0c5
khanhkiem <buidoigiauco@gmail.com>
2017-09-04 18:06:47 +0700

    Skip if output exist

--- Lab2/Exercises/pi/run_pi_omp_gnu.sh
+++ Lab2/Exercises/pi/run_pi_omp_gnu.sh
@@ -12,8 +12,14 @@ for T in 1 8 16 32 48
 do 
   for N in 1000000 10000000 100000000 200000000 300000000 400000000 500000000
   do
-    printf '\n********************\n\n'
-    printf "$program $N $T\n\n"
-    `dirname $0`/$program $N $T  | tee `dirname $0`/$output_dir/$program-$N-$T
+    process_output_dir=`dirname $0`/$output_dir/$program-$N-$T
+    if [ -e process_output_dir ]
+    then
+      continue
+    else
+      printf "\n********************\n\n"
+      printf "$program $N $T\n\n"
+      `dirname $0`/$program $N $T  | tee process_output_dir
+    fi
   done
 done
--- Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
+++ Lab2/Exercises/sgemm/run_sgemm_omp_gnu.sh
@@ -12,8 +12,13 @@ for T in 1 8 16 32 48
 do
   for N in 100 1000 2000 4000 6000 8000 10000
   do
-    printf "\n********************\n\n"
-    printf "$program $N $T\n\n"
-    `dirname $0`/$program $N $T  | tee `dirname $0`/$output_dir/$program-$N-$T
+    process_output_dir=`dirname $0`/$output_dir/$program-$N-$T
+    if [ -e process_output_dir ]
+    then
+      continue
+    else
+      printf "\n********************\n\n"
+      printf "$program $N $T\n\n"
+      `dirname $0`/$program $N $T  | tee process_output_dir
   done
 done
