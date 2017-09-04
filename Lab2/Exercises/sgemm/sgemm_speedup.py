10317b21637a07f89964bc9241da2c10d3f7376d
khanhkiem <buidoigiauco@gmail.com>
2017-09-04 10:37:13 +0700

    Reach dime = 4000 for 8 -> 48 threads

--- Lab2/Exercises/sgemm/sgemm_speedup.py
+++ Lab2/Exercises/sgemm/sgemm_speedup.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 def main():
     plt.figure(figsize=(12, 7))
 
-    # results from serial version of pi calculation
-    sgemm_serial = [0.000929, 1.1194, 8.62048, 180.573, ]
+    # results from serial version of sgemm calculation
+    sgemm_serial = [0.011164, 14.5412,  109.243, ]
 
     # results from openmp on host version of sgemm calculation: 8 threads
-    sgemm_omp_8threads = []
+    sgemm_omp_8threads = [0.00177, 2.2704, 18.6356, 180.404]
 
     # results from openmp on host version of sgemm calculation: 16 threads
-    sgemm_omp_16threads = []
+    sgemm_omp_16threads = [0.001207, 1.50885,  11.2423, 68.6579]
 
     # results from openmp on host version of sgemm calculation: 32 threads
-    sgemm_omp_32threads = []
+    sgemm_omp_32threads = [0.013416,  0.958614, 7.38648,  59.5107]
 
     # results from openmp on host version of sgemm calculation: 48 threads
-    sgemm_omp_48threads = []
+    sgemm_omp_48threads = [0.020395,  0.786059, 5.32728,  60.4137]
 
     loops = ('100', '1000', '2000', '4000', '6000', '8000', '10000')
     x_pos = np.arange(len(sgemm_serial))
