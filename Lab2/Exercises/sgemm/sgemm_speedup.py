188db765de593242c9d5c3c5e93630fee19f09db
khanhkiem <buidoigiauco@gmail.com>
2017-09-06 05:44:31 +0700

    Add 6000, 8000, 10000 for each threads

--- Lab2/Exercises/sgemm/sgemm_speedup.py
+++ Lab2/Exercises/sgemm/sgemm_speedup.py
@@ -6,19 +6,24 @@ def main():
     plt.figure(figsize=(12, 7))
 
     # results from serial version of sgemm calculation
-    sgemm_serial = [0.011164, 14.5412,  109.243, 1095.86]
+    sgemm_serial = [0.011164, 14.5412,  109.243, 1095.86,
+                    3881.92, 6914.15, 9181.26]
 
     # results from openmp on host version of sgemm calculation: 8 threads
-    sgemm_omp_8threads = [0.00177, 2.2704, 18.6356, 180.404]
+    sgemm_omp_8threads = [0.00177, 2.2704, 18.6356, 180.404,
+                          242.411, 575.409, 1104.69]
 
     # results from openmp on host version of sgemm calculation: 16 threads
-    sgemm_omp_16threads = [0.001207, 1.50885,  11.2423, 68.6579]
+    sgemm_omp_16threads = [0.001207, 1.50885,  11.2423, 68.6579,
+                           120.325, 291.365, 561.505]
 
     # results from openmp on host version of sgemm calculation: 32 threads
-    sgemm_omp_32threads = [0.013416,  0.958614, 7.38648,  59.5107]
+    sgemm_omp_32threads = [0.013416,  0.958614, 7.38648,  59.5107,
+                           84.8492, 208.065, 399.984]
 
     # results from openmp on host version of sgemm calculation: 48 threads
-    sgemm_omp_48threads = [0.020395,  0.786059, 5.32728,  60.4137]
+    sgemm_omp_48threads = [0.020395,  0.786059, 5.32728,  60.4137,
+                           70.0501, 171.1, 325.873]
 
     loops = ('100', '1000', '2000', '4000', '6000', '8000', '10000')
     x_pos = np.arange(len(sgemm_serial))
